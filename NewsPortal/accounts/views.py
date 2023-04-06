5716a7b6440a71bae2b3215b54ccd5385911fcb4
“FanatikLeo2” <leonfar@yandex.ru>
2023-04-06 16:48:53 +0300

    added identification, authentication(yandex include), authorization, button of possible will be author

---
+++ NewsPortal/accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
+from News.models import Author
+
+
+class SignUp(CreateView):
+    model = User
+    form_class = SignUpForm
+    success_url = '/accounts/login'
+    template_name = 'registration/signup.html'
+
+
+@login_required
+def upgrade_user(request):
+    user = request.user
+    group = Group.objects.get(name='authors')
+    if not user.groups.filter(name='authors').exists():
+        group.user_set.add(user)
+        Author.objects.create(author_user=User.objects.get(pk=user.id))
+    return redirect('create_post')
